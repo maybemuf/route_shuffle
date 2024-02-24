e856a6b50f9ee38843228ad90b5e274c7bf96e02
Lesha <maybemuf@gmail.com>
2024-02-24 14:13:10 +0200

    added duration extension on int

---
+++ lib/core/utils/extensions.dart
@@ -0,0 +1,9 @@
+extension DurationExtension on int {
+  Duration get seconds => Duration(seconds: this);
+  Duration get minutes => Duration(minutes: this);
+  Duration get hours => Duration(hours: this);
+  Duration get days => Duration(days: this);
+  Duration get weeks => Duration(days: this * 7);
+  Duration get months => Duration(days: this * 30);
+  Duration get years => Duration(days: this * 365);
+}
