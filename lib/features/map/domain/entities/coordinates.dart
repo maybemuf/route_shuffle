8a3ee60525cfd65e00c432fc90b4ffe0fe768418
Lesha <maybemuf@gmail.com>
2024-02-24 11:33:29 +0200

    made coordinates object freezed

--- lib/features/map/domain/entities/coordinates.dart
+++ lib/features/map/domain/entities/coordinates.dart
@@ -1,10 +1,11 @@
-class Coordinates {
-  final double latitude;
-  final double longitude;
+import 'package:freezed_annotation/freezed_annotation.dart';
 
-  Coordinates({
-    required this.latitude,
-    required this.longitude,
-  });
+part 'generated/coordinates.freezed.dart';
+
+@freezed
+class Coordinates with _$Coordinates {
+  const factory Coordinates({
+    required double latitude,
+    required double longitude,
+  }) = _Coordinates;
 }
-
