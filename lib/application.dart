b2f46a8337611d319dcbea216a7dd95b9444247b
Lesha <maybemuf@gmail.com>
2024-02-24 13:53:30 +0200

    moved theme to ui folder

--- lib/application.dart
+++ lib/application.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:route_shuffle/core/router/router.dart';
-import 'package:route_shuffle/core/theme/theme.dart';
+import 'package:route_shuffle/core/ui/theme/theme.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
