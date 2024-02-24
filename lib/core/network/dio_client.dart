0a84fa26bf66a0a104b515e50f9cda363fe8e85b
Lesha <maybemuf@gmail.com>
2024-02-24 14:13:22 +0200

    implemented dio client

---
+++ lib/core/network/dio_client.dart
@@ -0,0 +1,111 @@
+import 'package:dio/dio.dart';
+import 'package:route_shuffle/core/network/interceptors.dart';
+import 'package:route_shuffle/core/utils/extensions.dart';
+
+class DioClient {
+  late final Dio _dio;
+
+  DioClient({required String baseUrl, required String key})
+      : _dio = Dio(
+          BaseOptions(
+            baseUrl: baseUrl,
+            queryParameters: {'key': key},
+            connectTimeout: 3000.seconds,
+            receiveTimeout: 5000.seconds,
+            headers: {'Content-Type': 'application/json; charset=UTF-8'},
+          ),
+        )..interceptors.addAll([LoggerInterceptor()]);
+
+  // GET METHOD
+  Future<Response<dynamic>> get(
+    String url, {
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.get<dynamic>(
+        url,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } on DioException {
+      rethrow;
+    }
+  }
+
+  // POST METHOD
+  Future<Response<dynamic>> post(
+    String url, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.post<dynamic>(
+        url,
+        data: data,
+        options: options,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // PUT METHOD
+  Future<Response<dynamic>> put(
+    String url, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+    ProgressCallback? onSendProgress,
+    ProgressCallback? onReceiveProgress,
+  }) async {
+    try {
+      final response = await _dio.put<dynamic>(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+        onSendProgress: onSendProgress,
+        onReceiveProgress: onReceiveProgress,
+      );
+      return response;
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+  // DELETE METHOD
+  Future<dynamic> delete(
+    String url, {
+    dynamic data,
+    Map<String, dynamic>? queryParameters,
+    Options? options,
+    CancelToken? cancelToken,
+  }) async {
+    try {
+      final response = await _dio.delete<dynamic>(
+        url,
+        data: data,
+        queryParameters: queryParameters,
+        options: options,
+        cancelToken: cancelToken,
+      );
+      return response.data;
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
