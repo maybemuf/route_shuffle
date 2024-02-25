30c4f1f991be8e9b8572c952a1dbc0295d9df6b1
Lesha <maybemuf@gmail.com>
2024-02-25 13:48:08 +0200

    implemented reverse geocoding

---
+++ lib/features/map/data/services/map_service.dart
@@ -0,0 +1,42 @@
+import 'package:dio/dio.dart';
+import 'package:route_shuffle/core/errors/exceptions.dart';
+import 'package:route_shuffle/core/network/dio_client.dart';
+import 'package:route_shuffle/features/map/domain/entities/coordinates.dart';
+import 'package:route_shuffle/features/map/domain/entities/geocoding_response.dart';
+
+abstract interface class MapService {
+  Future<GeocodingResponse> reverseGeocode(Coordinates coordinates);
+
+  Future<GeocodingResponse> geocode(String address);
+}
+
+class MapServiceImpl implements MapService {
+  final DioClient dioClient;
+
+  MapServiceImpl({required this.dioClient});
+
+  @override
+  Future<GeocodingResponse> reverseGeocode(Coordinates coordinates) async {
+    try {
+      final response = await dioClient.get(
+        '/geocode/json',
+        queryParameters: {
+          'latlng': '${coordinates.lat},${coordinates.lng}',
+          'location_type': 'ROOFTOP|APPROXIMATE',
+        },
+      );
+
+      return GeocodingResponse.fromJson(response.data as Map<String, dynamic>);
+    } on DioException catch (e) {
+      throw ApiException(
+        message: e.message ?? 'Something went wrong',
+        statusCode: e.response?.statusCode ?? 500,
+      );
+    }
+  }
+
+  @override
+  Future<GeocodingResponse> geocode(String address) async {
+    throw UnimplementedError();
+  }
+}
